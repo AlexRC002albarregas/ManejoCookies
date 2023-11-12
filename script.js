15d6fa77afefda48023fd291342f2fbc36647d42
Alejandro <areinac01@iesalbarregas.es>
2023-11-12 12:30:52 +0100

    Cambios

--- script.js
+++ script.js
@@ -1,4 +1,4 @@
-// Función para iniciar sesión
+// Función para el inicio de sesión
 function login() {
     const username = document.getElementById('username').value;
   
@@ -6,16 +6,16 @@ function login() {
     document.cookie = `${username}= ${generateRandomToken()}`;
     document.cookie = 'isLoggedIn=true';
   
-    // Crear cookie persistente con expiración en un mes
+    // Cookie persistente con expiración en un mes
     const expirationDate = new Date();
     expirationDate.setMonth(expirationDate.getMonth() + 1);
     document.cookie = `cesta de la compra=valor;expires=${expirationDate.toUTCString()}`;
   
-    // Redirigir a la página de dashboard
+    // Redirigir a la página siguiente al iniciar sesión
     window.location.href = 'pagina.html';
   }
   
-  // Función para cerrar sesión
+  // Función para el cierre de la sesión
   function logout() {
     // Cambiar el estado de isLoggedIn a falso
     document.cookie = 'isLoggedIn=false';
@@ -24,12 +24,12 @@ function login() {
     window.location.href = 'index.html';
   }
   
-  // Función para generar un token aleatorio de 64 caracteres
+  // Función para generar un valor aleatorio de 64 caracteres
   function generateRandomToken() {
     return [...Array(64)].map(() => Math.random().toString(36)[2]).join('');
   }
   
-  // Comprobar si la cookie isLoggedIn tiene valor true al cargar la página de inicio de sesión
+  // Comprobamos si la cookie isLoggedIn tiene valor true al cargar la página de inicio de sesión
   window.onload = function () {
     const isLoggedIn = getCookie('isLoggedIn');
   
